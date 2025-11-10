24c0c7c9e2cd36b3751d613e380bca5422a47a86
mbuiop <milad.yari1377m@gmail.com>
2025-11-10 22:09:44 +0330

    Update Android.mk

--- android/Android.mk
+++ android/Android.mk
@@ -6,15 +6,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := SDL3
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SRC_FILES := \
-    SDL_android.c \
-    SDL_androidgl.c \
-    SDL_androidmessagebox.c \
-    SDL_androidwindow.c \
-    SDL_androidvulkan.c
+    SDL_android.c
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 LOCAL_CFLAGS := -DGL_GLEXT_PROTOTYPES
-LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -landroid
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -23,20 +18,22 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := kohksh_android
 
-# فایل‌های منبع پروژه شما
+# فایل‌های منبع پروژه - همه فایل‌های موجود
 LOCAL_SRC_FILES := \
-    ../main.cpp \
-    ../kohksh.cpp \
-    $(wildcard ../src/*.cpp) \
-    $(wildcard ../src/*.c)
+    main.cpp
+
+# اگر فایل‌های دیگری وجود دارد، اضافه کنید
+LOCAL_SRC_FILES += $(wildcard ../*.cpp)
+LOCAL_SRC_FILES += $(wildcard ../*.c)
+LOCAL_SRC_FILES += $(wildcard ../src/*.cpp)
+LOCAL_SRC_FILES += $(wildcard ../src/*.c)
 
 # شامل کردن SDL
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/include \
-    $(LOCAL_PATH)/../src
+    $(LOCAL_PATH)/include
 
 LOCAL_STATIC_LIBRARIES := SDL3
 LOCAL_LDLIBS := -llog -landroid -lGLESv1_CM -lGLESv2
-LOCAL_CFLAGS := -DANDROID -DSDL_ANDROID
+LOCAL_CFLAGS := -DANDROID
 
 include $(BUILD_SHARED_LIBRARY)
